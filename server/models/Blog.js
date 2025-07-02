cd2118dabb1269f19e8a0da98f2cc61f0be91e42
Ayna Gopakumar <aynag11@gmail.com>
2025-07-03 02:46:14 +0530

    Update: Adjust blog schema for edit permissions

--- server/models/Blog.js
+++ server/models/Blog.js
@@ -1,12 +1,14 @@
-// models/Blog.js
 const mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  title: String,
+  content: String,
   createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
+  author: { // 🔁 Rename this from "user" to "author"
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
 });
 
 module.exports = mongoose.model('Blog', blogSchema);
